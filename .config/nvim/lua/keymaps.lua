83889c1bcae3df7ff0483c409b631847fd320dc9
Bence R <bencethegreat@gmail.com>
2024-12-21 16:01:42 +0000

    Added moveline

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -29,6 +29,13 @@ map("n", "<leader>gg", function()
     Snacks.lazygit.open()
 end, { desc = "Open Lazygit" })
 
+-- MoveLine
+map("n", "<M-j>", ":MoveLine(1)<CR>", { desc = "Moves line up" })
+map("n", "<M-k>", ":MoveLine(-1)<CR>", { desc = "Moves line down" })
+
+map("v", "<M-j>", ":MoveBlock(1)<CR>", { desc = "Moves line up" })
+map("v", "<M-k>", ":MoveBlock(-1)<CR>", { desc = "Moves line down" })
+
 -- -- BARBAR
 -- Move to previous/next
 map("n", "<Tab>", "<Cmd>BufferNext<CR>")
